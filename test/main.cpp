af1b4456c2231326d285976db19c1ffb374b5324
Raymond Hammarling <raykay.rh@gmail.com>
2021-03-24 22:01:10 +0100

    Reformat tests

--- test/main.cpp
+++ test/main.cpp
@@ -1,48 +1,49 @@
 #define CATCH_CONFIG_MAIN
+
 #include <catch2/catch.hpp>
 
 #include "aabb.hpp"
 
 TEST_CASE("overlap b to right", "[aabb]")
 {
-    AABB a = {0, 0, 10, 10};
-    AABB b = {5, 3, 15, 13};
-    Fixed outX = 0, outY = 0;
+	AABB a = {0, 0, 10, 10};
+	AABB b = {5, 3, 15, 13};
+	Fixed outX = 0, outY = 0;
 
-    calculateOverlap(a, b, outX, outY);
+	calculateOverlap(a, b, outX, outY);
 
-    REQUIRE(outX == 5);
+	REQUIRE(outX == 5);
 }
 
 TEST_CASE("overlap b to left", "[aabb]")
 {
-    AABB a = {6, 0, 16, 10};
-    AABB b = {0, 0, 10, 10};
-    Fixed outX = 0, outY = 0;
+	AABB a = {6, 0, 16, 10};
+	AABB b = {0, 0, 10, 10};
+	Fixed outX = 0, outY = 0;
 
-    calculateOverlap(a, b, outX, outY);
+	calculateOverlap(a, b, outX, outY);
 
-    REQUIRE(outX == -4);
+	REQUIRE(outX == -4);
 }
 
 TEST_CASE("no overlap", "[aabb]")
 {
-    AABB a = {12, 0, 22, 10};
-    AABB b = {0, 0, 10, 10};
-    Fixed outX = 0, outY = 0;
+	AABB a = {12, 0, 22, 10};
+	AABB b = {0, 0, 10, 10};
+	Fixed outX = 0, outY = 0;
 
-    calculateOverlap(a, b, outX, outY);
+	calculateOverlap(a, b, outX, outY);
 
-    REQUIRE(outX == 0);
+	REQUIRE(outX == 0);
 }
 
 TEST_CASE("complete overlap", "[aabb]")
 {
-    AABB a = {0, 0, 10, 10};
-    AABB b = {0, 0, 10, 10};
-    Fixed outX = 0, outY = 0;
+	AABB a = {0, 0, 10, 10};
+	AABB b = {0, 0, 10, 10};
+	Fixed outX = 0, outY = 0;
 
-    calculateOverlap(a, b, outX, outY);
+	calculateOverlap(a, b, outX, outY);
 
-    REQUIRE((outX == 10 || outX == -10));
+	REQUIRE((outX == 10 || outX == -10));
 }
